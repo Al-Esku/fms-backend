266e365677b7bea5f4e148d88310fdc9febf2576
Alex Holton <aho139@uclive.ac.nz>
2024-05-30 03:20:14 +1200

    Added appspec and buildspec files, and scripts for running server

---
+++ scripts/server_start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd /home/ec2-user/server || exit
+
+source .env
+
+export DB_URL
+export DB_USERNAME
+export DB_PASSWORD
+export MAPBOX_KEY
+
+sudo java -jar *.jar \
+  --spring.config.additional-location=classpath:/production.properties
